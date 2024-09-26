da8b505a77307bc2c8289daad48b842c19565aa5
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-09-26 12:16:56 -0400

    feat: add delete method with authorization policy

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -78,6 +78,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        Gate::authorize('delete', $chirp);
+ 
+        $chirp->delete();
+ 
+        return redirect(route('chirps.index'));
     }
 }
--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -43,9 +43,9 @@ class ChirpPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Chirp $chirp): void
+    public function delete(User $user, Chirp $chirp): bool
     {
-        //
+        return $this->update($user, $chirp);
     }
 
     /**
