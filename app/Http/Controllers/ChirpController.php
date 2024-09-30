40e5534e0cee6762940d4eed01d5e1a6bedfcbeb
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-09-30 12:48:21 -0400

    style: update layout, change links, and modify buttons

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -46,37 +46,25 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp): View
+    public function edit(Chirp $chirp): void
     {
-        Gate::authorize('update',$chirp);
-        return view('chirps.edit', [
-            'chirp' => $chirp,
-        ]);
+       //
         
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp):RedirectResponse
+    public function update(Request $request, Chirp $chirp):void
     {
-        Gate::authorize('update',$chirp); 
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
-        $chirp->update($validated);
-        return redirect(route('chirps.index'));  
+        //  
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Chirp $chirp)
+    public function destroy(Chirp $chirp): void   
     {
-        Gate::authorize('delete', $chirp);
- 
-        $chirp->delete();
- 
-        return redirect(route('chirps.index'));
+         //
     }
 }
